e00c46e31b86145747002cc58960f6cd1b63bfff
asherDn-Official <developers@asherdn.com>
2024-07-31 16:33:45 +0530

    UpdateGlobal

--- frontend/src/Components/global.js
+++ frontend/src/Components/global.js
@@ -1,5 +1,5 @@
 // const API_URL = "http://localhost:4002";
-// const API_URL = "http://103.14.120.38:4002";
-const API_URL = "http://51.20.71.183:4002/";
+const API_URL = "https://blog.revnitro.com";
+//const API_URL = "http://51.20.71.183:4002/";
 
 export default API_URL;
